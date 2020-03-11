426bcb4b2591d7da5130bec5b4c2681eecb7c279
Ehsan Mozaffari <ehsan.mozaffari@yahoo.com>
2020-03-11 15:10:48 +0330

    array lesson 2

--- src/main/scala/codility/ArraysLesson2.scala
+++ src/main/scala/codility/ArraysLesson2.scala
@@ -9,14 +9,14 @@ object ArraysLesson2 {
 
   def oddOccurrencesInArray(a: Array[Int]): Int = {
 
-    val accuranceArray = new Array[Int](10)
+    val occArr = new Array[Int](10)
 
     for (i <- 0 to a.length - 1 ) {
-      accuranceArray(a(i)) += 1
+      occArr(a(i)) += 1
     }
 
     (for {
-      j <- 0 to accuranceArray.length -1 if (accuranceArray(j) % 2 > 0)
+      j <- 0 to occArr.length -1 if (occArr(j) % 2 > 0)
     } yield {
       j
     }).headOption.getOrElse(-1)
